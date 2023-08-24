2561d6448af15803b2e2e99b3f769cf18b109726
Tucker-Evans-19 <evanst@mit.edu>
2023-08-24 11:46:01 -0400

    commenting out unnecessary sections of pxtd_analysis and providing three-channel analysis function

--- pxtd_analysis.py
+++ pxtd_analysis.py
@@ -85,7 +85,7 @@ def roll_average(vector_input, half_window):
 # MAIN FUNCTIONS FOR ANALYZING A RAW PXTD H5 FILE: 
 #--------------------------------------------------
 
-def analyzePXTD_2channel(file):
+def pxtd_lineouts_2channel(file):
     c1low = 181
     c1high = 189
 
@@ -207,6 +207,135 @@ def analyzePXTD_2channel(file):
 
     return time_axis, c1_lineout, c2_lineout, fid_lineout
 
+def pxtd_lineouts_3channel(file):
+
+    #defining regions of interest for the channels (assuming 3 channels)
+    c1low = 181
+    c1high = 189
+
+    c2low = 161
+    c2high = 168
+
+    c3low = 140
+    c3high = 150
+    #----------------------------------------------------------------
+    # FILE CONVERSION (if necessary)
+    #----------------------------------------------------------------
+
+    #making sure that this is in h5 format and if not, updating it. 
+    if file[-3:] == 'hdf':
+        print('Converting to h5 format')
+        os.system(f"./h4toh5 {file}")
+        file = file[:-4] + '.h5'
+    elif file[-3:] == '.h5':
+        print('Recognized file format')
+    else:
+        print('Not a recognized file format. Exiting.')
+        sys.exit()
+
+    print(f'analyzing {file}')
+
+    #---------------------------------------------------------------
+    # FILE INTERPRETATION
+    #---------------------------------------------------------------
+
+    #breaking out file information:
+    shot_num = file[-13:-7]
+    data = hp.File(file, 'r')
+    image_raw = np.array(data.get('Streak_array'))
+    image_clean = image_raw[0,:,:] - image_raw[1,:,:]
+    #breaking out the energy information 
+
+    try:
+        energy_file = sys.argv[2]
+
+        with open(energy_file, 'r') as energy_data:
+            header = energy_data.readline()
+            spec_line = energy_data.readline()
+            spec_info = spec_line.split()
+            d3hep_e_mean, d3hep_e_err, d3hep_sigma, d3hep_sigma_err, T_D, T_T = spec_info
+    except(IndexError):
+        pass 
+
+    #rotating the image (needed for all PXTD images)
+    image_rot = ndimage.rotate(image_clean, .0297*180/3.1415)
+
+    #taking blocks of full image that will be the basis of the lineouts once averaged
+    c1_lineout_block = ndimage.median_filter(image_rot[c1low:c1high,left_side:right_side], size = (med_filt1, med_filt2))
+    c2_lineout_block = ndimage.median_filter(image_rot[c2low:c2high,left_side:right_side], size = (med_filt1, med_filt2))
+    c3_lineout_block = ndimage.median_filter(image_rot[c3low:c3high,left_side:right_side], size = (med_filt1, med_filt2))
+    fid_lineout_block = ndimage.median_filter(image_rot[fidlow:fidhigh, left_side:right_side], size = (med_filt1, med_filt2))
+    #--------------------------------------------------
+    # PCOLOR IMAGE - lineout blocks
+    #--------------------------------------------------
+
+    #Plotting pcolor of each lineout block
+    #averaging to find lineout of each channel
+    c1_lineout = np.average(c1_lineout_block, 0)
+    c2_lineout = np.average(c2_lineout_block, 0)
+    c3_lineout = np.average(c3_lineout_block, 0)
+    fid_lineout = np.average(fid_lineout_block, 0)
+
+    c1_lineout -= min(c1_lineout) 
+    c2_lineout -= min(c2_lineout)
+
+    c1_lineout = c1_lineout/ np.max(c1_lineout)
+    c2_lineout = c2_lineout/ np.max(c2_lineout)
+
+    #----------------------------------------------------------
+    # FIDUCIAL PEAK TIMING 
+    #----------------------------------------------------------
+    #finding peaks in the fiducial lineout:
+    fid_cutoff = np.max(fid_lineout)*.5
+    fid_filter = (fid_lineout >= fid_cutoff)
+    norm_fid = fid_lineout/ np.max(fid_lineout)
+
+    #establish index ranges:
+    in_peak = False
+    left_sides = []
+    right_sides = []
+
+    for element in range(round(len(fid_filter)*.8)):
+        
+        if fid_filter[element] == 1:
+            if in_peak:
+                pass
+            else:
+                in_peak = True
+                left_sides.append(element)
+        else:
+            if in_peak:
+                in_peak = False
+                right_sides.append(element)
+            else:
+                pass
+
+    peak_segments = []
+    #for element in range(len(left_sides)):
+    #	peak_segments.append(fid_lineout[left_sides[element]:right_sides[element]])
+
+    peak_centers = []
+    peak_centers = .5*(np.array(left_sides) + np.array(right_sides))
+    
+    print('peak fiducial information: ')
+    print(peak_centers)
+    centers = peak_centers
+    #finding the average distance between the timing fiducial peaks: 
+    peak_diffs = []
+    for element in range(1,len(centers)):
+        peak_diffs.append(centers[element] - centers[element-1])
+
+    average_peak_diff = np.average(peak_diffs)
+
+    t_params, t_cov = curve_fit(timeAxis, centers, 548*np.arange(len(centers)))
+    print(t_params)
+    a,b,c = t_params
+    time_axis = timeAxis(np.arange(len(c2_lineout)),a,b,c)
+    time_axis -= min(time_axis)
+    time_per_pixel = 548/average_peak_diff
+
+    return time_axis, c1_lineout, c2_lineout, c3_lineout, fid_lineout
+'''
 def analyzePXTD(file, num_channels = 2):
     #--------------------------------------------------------------
     #SETTING NUMBER OF CHANNELS:
@@ -220,8 +349,6 @@ def analyzePXTD(file, num_channels = 2):
         c2low = 161
         c2high = 168
 
-        c3low = 140
-        c3high = 150
     elif num_channels ==3:
         #defining regions of interest for the channels (assuming 3 channels)
         c1low = 181
@@ -288,7 +415,8 @@ def analyzePXTD(file, num_channels = 2):
     #taking blocks of full image that will be the basis of the lineouts once averaged
     c1_lineout_block = ndimage.median_filter(image_rot[c1low:c1high,left_side:right_side], size = (med_filt1, med_filt2))
     c2_lineout_block = ndimage.median_filter(image_rot[c2low:c2high,left_side:right_side], size = (med_filt1, med_filt2))
-    c3_lineout_block = ndimage.median_filter(image_rot[c3low:c3high,left_side:right_side], size = (med_filt1, med_filt2))
+    if num_channels == 3: 
+        c3_lineout_block = ndimage.median_filter(image_rot[c3low:c3high,left_side:right_side], size = (med_filt1, med_filt2))
     fid_lineout_block = ndimage.median_filter(image_rot[fidlow:fidhigh, left_side:right_side], size = (med_filt1, med_filt2))
     #--------------------------------------------------
     # PCOLOR IMAGE - lineout blocks
@@ -298,7 +426,8 @@ def analyzePXTD(file, num_channels = 2):
     #averaging to find lineout of each channel
     c1_lineout = np.average(c1_lineout_block, 0)
     c2_lineout = np.average(c2_lineout_block, 0)
-    c3_lineout = np.average(c3_lineout_block, 0)
+    if num_channels ==3:
+        c3_lineout = np.average(c3_lineout_block, 0)
 
     fid_lineout = np.average(fid_lineout_block, 0)
 
@@ -321,7 +450,6 @@ def analyzePXTD(file, num_channels = 2):
     in_peak = False
     left_sides = []
     right_sides = []
-
 
     for element in range(round(len(fid_filter)*.8)):
         
@@ -605,3 +733,5 @@ def analyzePXTD_energy(file, num_channels = 2):
     xls2[-1] = 0
 
     return time_axis, xls1, xls2, fit1, fit2, shot_num, image_rot
+
+'''
